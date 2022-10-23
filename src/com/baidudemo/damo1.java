ee1dbaed19c34d4a3c9ab96624896df2eaa87d3a
buzhiyu <528066049@qq.com>
2022-10-23 14:40:06 +0800

    test

---
+++ src/com/baidudemo/damo1.java
@@ -0,0 +1,9 @@
+package com.baidudemo;
+
+public class damo1 {
+    public static void main(String[] args) {
+        System.out.println(3 + 2);
+        System.out.println("hellow world");
+
+    }
+}
---
+++ src/com/baidudemo/helloWorld.java
@@ -0,0 +1,8 @@
+package com.baidudemo;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+    }
+}
+//注释
